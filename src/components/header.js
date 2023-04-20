db9829179cbffed8e240d5a4684b0ddb3c372fcc
Zonary123 <carlosvarasalonso10@gmail.com>
2023-04-20 03:40:41 +0200

    Composicion del Header
    Probando cosas

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,34 @@
 import React, { useState } from 'react'
+import './header.css'
 
 function Header () {
   const [darkMode, setDarkMode] = useState(false)
 
   const ChangeTheme = () => {
     setDarkMode(!darkMode)
-    const body = document.querySelector('body')
-    body.classList.toggle('dark')
+    const html = document.querySelector('html')
+    html.classList.toggle('dark')
   }
   return (
-    <div className='Header font-bold bg-white dark:bg-[#20252c] dark:text-white'>
-      <h1>Header</h1>
-      <button
-        onClick={ChangeTheme}
-        className='Theme dark:bg-white dark:text-black m-2 p-2 rounded-lg border-slate-400 border-y-2 border-x-2'
-      >
-        Theme
-      </button>
+    <div className='Header font-bold bg-white dark:bg-[#22252c] dark:text-white '>
+      <div>
+        <img src='./logo.svg' alt='' className='logo' />
+      </div>
+      <div className='Search'>
+        <input
+          type='search'
+          className=' rounded-lg bg-gray-200 border-gray-400 border-y-2 border-x-2 dark:bg-white'
+        />
+      </div>
+      <div>
+        <span>sign up</span>
+        <button
+          onClick={ChangeTheme}
+          className='Theme dark:bg-white dark:text-black m-2 p-2 rounded-lg border-slate-400 border-y-2 border-x-2'
+        >
+          Theme
+        </button>
+      </div>
     </div>
   )
 }
