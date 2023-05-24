31f087916a8458245b6e64a8a5bc1d899d0a7a3a
CarlosVarasAlonso <carlos.varalo@educa.jcyl.es>
2023-05-24 20:10:32 +0200

    Cambio boton theme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 import './css/Footer.css'
+
 function Footer () {
   return (
-    <footer>
-      <div class='container'>
-        <div class='footer-content'>
-          <p>© 2023 Ejemplo de Página Web. Todos los derechos reservados.</p>
-          <ul class='footer-links'>
+    <footer className='fixed bottom-0 left-0 w-full bg-gray-200 py-4 dark:bg-[#22252c] '>
+      <div className='container mx-auto'>
+        <div className='footer-content flex justify-center items-center'>
+          <p className='text-gray-600 dark:text-gray-200 text-sm'>
+            © 2023 Ejemplo de Página Web. Todos los derechos reservados.
+          </p>
+          <ul className='footer-links ml-4'>
             <li>
-              <Link to='/' className=" text-cyan-600 ">Inicio</Link>
+              <Link to='/' className='text-gray-600 dark:text-gray-200'>
+                Inicio
+              </Link>
             </li>
             <li>
-            <Link to='/acerca_de' className=" text-cyan-600 ">Acerca de</Link>
+              <Link to='/acerca_de' className='text-gray-600 dark:text-gray-200'>
+                Acerca de
+              </Link>
             </li>
             <li>
-              <a href='#'>Servicios</a>
+              <a href='#' className='text-gray-600 dark:text-gray-200'>
+                Servicios
+              </a>
             </li>
             <li>
-              <Link to={'/contacto'} className=" text-cyan-600 ">Contacto</Link>
+              <Link to='/contacto' className='text-gray-600 dark:text-gray-200'>
+                Contacto
+              </Link>
             </li>
           </ul>
         </div>
@@ -25,4 +36,5 @@ function Footer () {
     </footer>
   )
 }
+
 export default Footer
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './header.css'
 import { Link } from 'react-router-dom'
+import { FaMoon, FaSun } from 'react-icons/fa'
 
 function Header () {
   const [darkMode, setDarkMode] = useState(false)
@@ -11,14 +12,14 @@ function Header () {
     html.classList.toggle('dark')
   }
   return (
-    <div className='Header font-bold bg-white dark:bg-[#22252c] dark:text-white mb-4'>
+    <div className='Header font-bold bg-gray-200 dark:bg-[#22252c] dark:text-white mb-4'>
       <div>
         <img src='./logo.svg' alt='' className='logo' />
       </div>
       <div className='Search'>
         <input
           type='search'
-          className=' rounded-lg bg-gray-200 border-gray-400 border-y-2 border-x-2 dark:bg-white'
+          className=' rounded-lg bg-gray-100 border-gray-400 border-y-2 border-x-2 dark:bg-white'
         />
       </div>
       <div>
@@ -29,7 +30,7 @@ function Header () {
           onClick={ChangeTheme}
           className='Theme dark:bg-white dark:text-black m-2 p-2 rounded-lg border-slate-400 border-y-2 border-x-2'
         >
-          Theme
+          {darkMode ? <FaMoon size={20} /> : <FaSun size={20} />}
         </button>
       </div>
     </div>
